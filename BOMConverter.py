2e391035c8ddcb1ffa39b2440924bebd6463ed68
hakkache <hakkache.mohamed@gmail.com>
2018-10-31 19:12:27 -0400

    Add files via upload

--- BOMConverter.py
+++ BOMConverter.py
@@ -1,7 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 18:56:18 2018
+
+@author: hakkache
+"""
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv('KK52-14B563-ZD.csv')
+data = pd.read_csv('KK52-14B563-ZD.csv',error_bad_lines=False)
 
 data.head()
 
@@ -15,7 +22,7 @@ def containAllWord(text, words):
             return False
     return True
 
-for i in range(73):
+for i in range(len(data.index)):
     if (containAllWord(data['column1'][i],['99999ITEM', ' Wire'])):
             data['column1'][i] =data['column1'][i].replace('99999ITEM','5ITEM')
     if (containAllWord(data['column1'][i],['99999ITEM', ' Terminal'])):
@@ -31,8 +38,9 @@ for i in range(73):
     if (containAllWord(data['column1'][i],['99999ITEM', 'Tube'])):
             data['column1'][i] =data['column1'][i].replace('99999ITEM','30ITEM')
 
-for i in range(73):
+for i in range(len(data.index)):
     print(data['column1'][i])
 data.columns = tmp
 
-data.to_csv('out.csv')
+data.to_csv('hakkache.csv')
+
